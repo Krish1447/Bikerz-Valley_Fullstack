c6d9f78dcc5aa148aec9cfc8ff0e3941642a67fb
Krish1447 <ridershark69@gmail.com>
2024-08-15 16:06:52 +0530

    Add files via upload

---
+++ Front/src/Components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        
+        <div>
+          <div className="hero-hand-icon">
+            
+           
+          </div>  
+          <div className="hero-hand-video">
+           
+       </div>
+        </div>
+        
+       
+      </div>
+      
+    </div>
+  );
+};
+
+
+export default Hero;
